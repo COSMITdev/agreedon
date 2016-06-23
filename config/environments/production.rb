7ce9dedccec562d681109f031d87ffe6d4be51ae
Patrick Müller <patrick.e.muller@gmail.com>
2016-06-23 18:40:33 -0300

    Fixes production options for default_url_host & asset_host paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,10 +9,12 @@ Rails.application.configure do
   config.assets.compile = false
   config.assets.digest = true
   config.log_level = :debug
-  config.action_controller.asset_host = 'https://agree-me.herokuapp.com'
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://agree-me.herokuapp.com' }
+  config.action_mailer.asset_host = ("https://agree-me-production.herokuapp.com")
+  config.action_controller.asset_host = "https://agree-me-production.herokuapp.com"
+  config.action_mailer.default_url_options = { host: "agree-me-production.herokuapp.com" }
+  config.action_controller.default_url_options = { host: "agree-me-production.herokuapp.com" }
 end
